e6b42dcb31eca70617cc6cba86d7201e6f7b7c7f
Roddy Rios <ext.rrios@gdmseeds.com>
2023-12-05 19:33:14 -0500

    first comit
    base

---
+++ Controllers/ElsaDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElsaDemoV01.Controllers
+{
+    public class ElsaDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
