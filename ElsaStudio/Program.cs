d5baf4f679554db2704319a8b07c7fcace023a6f
Julian Di Risio <julian.dirisio@gmail.com>
2024-01-29 15:51:58 -0300

    fix program

--- ElsaStudio/Program.cs
+++ ElsaStudio/Program.cs
@@ -45,8 +45,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseResponseCompression();
-
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
